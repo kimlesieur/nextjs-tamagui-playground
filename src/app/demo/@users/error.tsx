607568aa38980dd2d3a601a5028fdbc99bb660f2
Kim <k.lesieur@beapp.fr>
2024-01-28 19:39:51 +0100

    feat: add error handling with fake throwing error

---
+++ src/app/demo/@users/error.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function UsersError() {
+    return <h3>Failed to load users</h3>;
+}
--- src/app/demo/@users/page.tsx
+++ src/app/demo/@users/page.tsx
@@ -2,5 +2,6 @@ import { wait } from "@/lib/wait";
 
 export default async function Users() {
     await wait(3000);
+    throw new Error("Fail");
     return <div>Users</div>;
 }
